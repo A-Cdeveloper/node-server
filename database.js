539fc44cb436d3277d34fa050467a8c04cd30878
A-Cdeveloper <aleksandar@e-vlasotince.info>
2023-03-24 23:30:21 +0100

    timezone settings

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const pool = mysql
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    timezone: "+02:00",
+    timezone: "+01:00",
   })
   .promise();
 
